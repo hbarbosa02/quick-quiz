9c5e0e1b81d31da65105d32ee6ca8e9d70cdb556
Hiran Gondim <hiran.g.b.silva@gmail.com>
2020-03-25 16:59:45 -0300

    authenticação adicionado

--- src/app/graphql/questions/resolvers.js
+++ src/app/graphql/questions/resolvers.js
@@ -2,6 +2,8 @@ import Theme from '../../models/Theme'
 import Level from '../../models/Level'
 import Question from '../../models/Question'
 
+import { authenticated } from '../../../util/composables'
+
 export default {
   Question: {
     theme: parent => Theme.findByPk(parent.themeId),
@@ -10,10 +12,14 @@ export default {
   },
 
   Query: {
-    questions: () => Question.findAll({ where: { isActivated: true } }),
-    questionsByTheme: (_, args) =>
-      Question.findAll({ where: { themeId: args.theme, isActivated: true } }),
-    questionsByLevel: (_, args) =>
-      Question.findAll({ where: { levelId: args.level, isActivated: true } }),
+    questions: authenticated(() =>
+      Question.findAll({ where: { isActivated: true } })
+    ),
+    questionsByTheme: authenticated((_, args) =>
+      Question.findAll({ where: { themeId: args.theme, isActivated: true } })
+    ),
+    questionsByLevel: authenticated((_, args) =>
+      Question.findAll({ where: { levelId: args.level, isActivated: true } })
+    ),
   },
 }
