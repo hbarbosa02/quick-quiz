2dd6bb461df183df147a32fba44807b5a2b4b322
Hiran Gondim <hiran.g.b.silva@gmail.com>
2020-03-26 11:46:01 -0300

    queries ranking e rankings adicionadas

--- src/app/graphql/rankings/resolvers.js
+++ src/app/graphql/rankings/resolvers.js
@@ -8,6 +8,21 @@ export default {
     user: parent => User.findByPk(parent.userId),
   },
 
+  Query: {
+    ranking: authenticated((_, __, ctx) =>
+      Ranking.findOne({ where: { userId: ctx.user.id } })
+    ),
+
+    rankings: authenticated((_, args, ctx) =>
+      Ranking.findAll({
+        where: { mode: args.mode, userId: ctx.user.id },
+        order: [['score', 'DESC']],
+        limit: 10,
+        offset: 1,
+      })
+    ),
+  },
+
   Mutation: {
     saveScore: authenticated((_, args, ctx) =>
       Ranking.create({
